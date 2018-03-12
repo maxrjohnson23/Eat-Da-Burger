60fad4b07fb8a94b8534b70e5914b00c5e3cb9cc
Max <lfmax223@gmail.com>
2018-03-12 09:17:41 -0500

    Refactor client side javascript

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,30 +1,42 @@
-function devourBurger(id) {
-    $.ajax({
-        url: `/api/burger/${id}`,
-        type: "PUT",
-        success: function () {
-            window.location.reload();
-        }
-    })
-}
+const app = {
+    devourBurger: function (id) {
+        $.ajax({
+            url: `/api/burger/${id}`,
+            type: "PUT",
+            success: function () {
+                window.location.reload();
+            }
+        })
+    },
 
-$(".devour-btn").on("click", function () {
-    let id = $(this).attr("data-burger");
-    devourBurger(id);
-});
+    bindUIActions: function () {
+        $(".devour-btn").on("click", function () {
+            let id = $(this).attr("data-burger");
+            app.devourBurger(id);
+        });
 
-$("#addBurger").on("click", function (e) {
-    e.preventDefault();
-    const data = {
-        name: $("#burgerName").val()
-    };
-    $.ajax({
-        type: "POST",
-        url: "api/burger",
-        data: data,
-        success: function (result) {
-            console.log(result);
-            window.location.reload();
-        }
-    });
+        $("#addBurger").on("click", function (e) {
+            e.preventDefault();
+            const data = {
+                name: $("#burgerName").val()
+            };
+            $.ajax({
+                type: "POST",
+                url: "api/burger",
+                data: data,
+                success: function (result) {
+                    console.log(result);
+                    window.location.reload();
+                }
+            });
+        });
+    },
+
+    init: function () {
+        this.bindUIActions();
+    }
+};
+
+$(document).ready(function() {
+   app.init();
 });
